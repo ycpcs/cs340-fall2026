1815d923398512eda34fc41555590ebfe62c40f1
dchen3-ycp <dchen3@ycp.edu>
2026-08-03 21:26:01 -0400

    Update AACONFIG-scheduleInfo.js

--- js/AACONFIG-scheduleInfo.js
+++ js/AACONFIG-scheduleInfo.js
@@ -129,8 +129,7 @@ courseInfo.classPeriods = [
 // The following is for the college-scheduled final exam.
 // It is not used if final is on last day of class
 courseInfo.finalExamDates = [
-		new FinalExamDay("101", new Date("12/15/2018 08:00:00")),
-		new FinalExamDay("102", new Date("12/13/2018 10:15:00")),
+		new FinalExamDay("101", new Date("12/07/2026 08:00:00")),
 ];
 
 // vim:ts=2:
